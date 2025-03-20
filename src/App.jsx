29b683f8a0bbc52182bd8f9f79169f15ddebcec6
Boga Keerthi <keerthiboga2004@gmail.com>
2025-03-20 22:41:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,11 @@ import ImageSearch from './ImageSearch'
 import './App.css'
 
 function App() {
-<<<<<<< HEAD
+
   const apikey=import.meta.env.VITE_API_KEY;
   return (
     <>
       <ImageSearch apikey={apikey} />
-=======
-
-  return (
-    <>
-      <ImageSearch />
->>>>>>> 09c77dc77206a297485b2acd8cf983445c2a8103
     </>
   )
 }
